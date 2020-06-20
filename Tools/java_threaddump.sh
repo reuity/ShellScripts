6d3ce9e8ce6a850edc0f6833d20c84728c779b1a
reuity <123@163.com>
2020-06-21 01:58:59 +0800

    no msg

--- Tools/java_threaddump.sh
+++ Tools/java_threaddump.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-# use "top -c" find pid with high CPU usage
+# !!!!!!!
+# first use "top -c" find pid with high CPU usage
 # then sh $0 pid
 
 # check usage
@@ -19,7 +20,7 @@ fi
 # get variable
 pid=$1
 linenum=$2
-date=$(date +%H%M%S)
+date=$(date +%H%M)
 
 # check if pid exists
 if test -z "$(${JAVA_HOME}/bin/jps -l | cut -d '' -f 1 | grep ${pid})"; then
@@ -41,10 +42,12 @@ ${JAVA_HOME}/bin/jstack -l ${pid} >${stackfile}
 ps -mp ${pid} -o THREAD,tid,time | sort -k2r | awk '{if ($1 !="USER" && $2 != "0.0" && $8 !="-") print $8;}' | xargs printf "%x\n" >${threadsid}
 
 # print exception dump
-echo -e "\n-------------------------------------\n" | tee -a ${exceptionfile}
+count=1
 for i in $(cat ${threadsid}); do
     if [ $i != 0 ]; then
+        echo -e "\n---------------${count}---------------\n" | tee -a ${exceptionfile}
         grep ${i} -A ${linenum} ${stackfile} | tee -a ${exceptionfile}
-        echo -e "\n-------------------------------------\n" | tee -a ${exceptionfile}
     fi
+    count=$((${count} + 1))
 done
+echo -e "\n---------------END---------------\n" | tee -a ${exceptionfile}
